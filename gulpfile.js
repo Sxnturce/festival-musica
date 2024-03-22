cff1a5be13e06fdf9664a4f5a4643696eb992fab
SebastianSR <102806578+Sxnturce@users.noreply.github.com>
2024-03-22 12:39:24 -0500

    Agregar imagenes webp con gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
-const { src, dest, watch } = require("gulp")
+const { src, dest, watch, parallel } = require("gulp")
+
+//CSS
 const sass = require('gulp-sass')(require('sass'));
+
+//Errores
 const plumber = require("gulp-plumber")
 
+//Imagenes
+
+const webp = require("gulp-webp")
 
 function css(cb) {
     src("src/scss/**/*.scss")//Importar el aarchivo sass
@@ -11,10 +18,22 @@ function css(cb) {
     cb();
 }
 
+
+function versionWebp(cb) {
+    const options = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(options))
+        .pipe(dest("build/img"))
+    cb()
+}
+
 function dev(cb) {
     watch("src/scss/**/*.scss", css)
     cb();
 }
 
 exports.css = css
-exports.dev = dev
+exports.versionWebp = versionWebp
+exports.dev = parallel(versionWebp, dev)
